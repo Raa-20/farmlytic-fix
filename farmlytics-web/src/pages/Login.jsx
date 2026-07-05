55bba782e11570df9a205f67b7a4c18f690b64e8
Rohiyal A <248389818+Raa-20@users.noreply.github.com>
2026-07-05 17:13:00 +0700

    up

--- farmlytics-web/src/pages/Login.jsx
+++ farmlytics-web/src/pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login({ setIsLogin }) {
     setLoading(true);
 
     try {
-      const response = await fetch("https://api-farmlytics.tifpsdku.com/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/login`,{
         method: "POST",
         headers: {
           "Content-Type": "application/json",
