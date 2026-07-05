66f56a0947309ac04e730a491a152342681f5bd3
Rohiyal A <248389818+Raa-20@users.noreply.github.com>
2026-07-05 19:30:02 +0700

    up

--- farmlytics-web/src/pages/Login.jsx
+++ farmlytics-web/src/pages/Login.jsx
@@ -20,8 +20,9 @@ export default function Login({ setIsLogin }) {
     setLoading(true);
 
     try {
-      const response = await fetch('${import.meta.env.VITE_API_URL}/login',{
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
         method: "POST",
+        
         headers: {
           "Content-Type": "application/json",
         },
