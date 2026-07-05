c19d10c730ff3de25b85e1dfded507258150a2fd
Rohiyal A <248389818+Raa-20@users.noreply.github.com>
2026-07-05 17:04:24 +0700

    up

--- farmlytics-web/vite.config.js
+++ farmlytics-web/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // (atau sesuaikan dengan plugin yang kamu pakai)
 
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    host: true, // Wajib agar bisa diakses dari luar container Docker
+    allowedHosts: ['farmlytics-web.up.railway.app'], // Tambahkan baris ini
+  }
+})
