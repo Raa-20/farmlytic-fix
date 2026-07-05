b96b2dd43138c3a8a8fbde90e3de6d7efe2bb47b
Rohiyal A <248389818+Raa-20@users.noreply.github.com>
2026-07-05 18:23:11 +0700

    up

--- farmlytics-db/backend/config.py
+++ farmlytics-db/backend/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     # Mengambil data dari variabel Railway, dengan nilai cadangan (fallback) untuk di laptop
     DB_USER = os.getenv('MYSQLUSER', 'root')
-    DB_PASS = os.getenv('MYSQLPASSWORD', 'lpMTySusCyimDrzNOhpovwhGpGtPJAcQ')
+    DB_PASS = os.getenv('MYSQLPASSWORD', 'password123')
     DB_HOST = os.getenv('MYSQLHOST', 'mysql.railway.internal')
     DB_PORT = os.getenv('MYSQLPORT', '3306')
     DB_NAME = os.getenv('MYSQLDATABASE', 'railway')
