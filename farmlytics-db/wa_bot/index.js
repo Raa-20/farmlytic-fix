0ad426c77ca44c45679da79ee4fea986a38e7f2d
Rohiyal A <248389818+Raa-20@users.noreply.github.com>
2026-07-05 23:34:31 +0700

    baru

--- farmlytics-db/wa_bot/index.js
+++ farmlytics-db/wa_bot/index.js
@@ -7,7 +7,16 @@ const client = new Client({
   authStrategy: new LocalAuth({ clientId: "LADENTRA_BOT" }),
   puppeteer: {
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    timeout: 60000, 
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage", 
+      "--disable-accelerated-2d-canvas", 
+      "--no-first-run",
+      "--no-zygote",
+      "--disable-gpu" 
+    ],
   },
 });
 
