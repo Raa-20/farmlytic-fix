e749186dd0ac72660852b3a97419d612d11fe203
Rohiyal A <248389818+Raa-20@users.noreply.github.com>
2026-07-05 18:19:12 +0700

    up

--- farmlytics-db/backend/config.py
+++ farmlytics-db/backend/config.py
@@ -3,10 +3,10 @@ import os
 class Config:
     # Mengambil data dari variabel Railway, dengan nilai cadangan (fallback) untuk di laptop
     DB_USER = os.getenv('MYSQLUSER', 'root')
-    DB_PASS = os.getenv('MYSQLPASSWORD', 'password123')
-    DB_HOST = os.getenv('MYSQLHOST', 'db_mysql')
+    DB_PASS = os.getenv('MYSQLPASSWORD', 'lpMTySusCyimDrzNOhpovwhGpGtPJAcQ')
+    DB_HOST = os.getenv('MYSQLHOST', 'mysql.railway.internal')
     DB_PORT = os.getenv('MYSQLPORT', '3306')
-    DB_NAME = os.getenv('MYSQLDATABASE', 'farmlytics_db')
+    DB_NAME = os.getenv('MYSQLDATABASE', 'railway')
 
     # Merakit URL database sesuai dengan driver yang kamu pakai (mysqlconnector)
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
